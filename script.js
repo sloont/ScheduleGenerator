ad52b349bef4ea50b24b052c6f19f75419a584cf
Sloont <colinabel93@gmail.com>
2021-04-26 15:02:51 -0400

    added icon resources as well as inline svgs for testing with icons and recoloring. saved color values as comments in script.js

--- script.js
+++ script.js
@@ -23,8 +23,16 @@ const team5 = new Team(param1, param2, param3, param4);
 const team6 = new Team(param1, param2, param3, param4);
 const team7 = new Team(param1, param2, param3, param4);
 const team8 = new Team(param1, param2, param3, param4);
+/*
+<option value="1e90ff">Blue</option>
+<option value="#b22222">Red</option>
+<option value="#3cb371">Green</option>
+<option value="#ba55d3">Purple</option>
 
-
-
+<option value="#ffd700">Yellow</option>
+<option value="#ff7f50">Orange</option>
+<option value="#87ceeb">Light Blue</option>
+<option value="#fffafa">White</option>
+*/
 
 
