766d5094181ed2ebaaf6ae10f97c904017bc1cb5
Sloont <colinabel93@gmail.com>
2021-04-26 14:04:33 -0400

    added the outlines for css and html

--- script.js
+++ script.js
@@ -8,18 +8,21 @@ const Team = (teamName, logo, color, lastSeasonPlace) => {
     this.needsHomeGame = false;
     //this.canHaveBye = true;   lets not worry about byes for now
 
+    this.schedule = {};
+
+
     this.scheduleDifficulty = 0;
 }
 
-const team1 = new Team(param1, param2, param3);
-const team2 = new Team(param1, param2, param3);
-const team3 = new Team(param1, param2, param3);
-const team4 = new Team(param1, param2, param3);
+const team1 = new Team(param1, param2, param3, param4);
+const team2 = new Team(param1, param2, param3, param4);
+const team3 = new Team(param1, param2, param3, param4);
+const team4 = new Team(param1, param2, param3, param4);
 
-const team5 = new Team(param1, param2, param3);
-const team6 = new Team(param1, param2, param3);
-const team7 = new Team(param1, param2, param3);
-const team8 = new Team(param1, param2, param3);
+const team5 = new Team(param1, param2, param3, param4);
+const team6 = new Team(param1, param2, param3, param4);
+const team7 = new Team(param1, param2, param3, param4);
+const team8 = new Team(param1, param2, param3, param4);
 
 
 
