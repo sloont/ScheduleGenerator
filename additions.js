f23cb7da097055bf75d02672a55bbc9b973ad234
Sloont <colinabel93@gmail.com>
2021-04-27 12:43:32 -0400

    pretty rough design but successfully cloned the svg and moved it to the right place. currently moving the wrong svg, though

---
+++ additions.js
@@ -0,0 +1,35 @@
+const testFunc = () => {
+
+    const testContainer = document.createElement("div");
+    testContainer.classList.add("testContainer");
+
+    const logo = document.getElementById("benderSVG");
+    clonePath(logo);
+    logo.classList.add("logoChoice");
+
+    const logoBack = document.createElement("div");
+    logoBack.classList.add("logoBack");
+
+    const testDiv = document.getElementById("test");
+
+    testDiv.appendChild(testContainer);
+    
+    testContainer.appendChild(logo);
+    testContainer.appendChild(logoBack);
+
+};
+
+const testBtn = document.getElementById("testBtn");
+
+testBtn.addEventListener("click", () => {
+    testFunc();
+});
+
+let cloneCount = 0;
+const clonePath = (nodeObject) => {
+    const clonedPath = nodeObject.cloneNode(true);
+    cloneCount++;
+    clonedPath.id = clonedPath.id + cloneCount;
+    const parent = nodeObject.parentElement;
+    parent.appendChild(clonedPath)
+}
