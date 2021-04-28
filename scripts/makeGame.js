6af4f793f95862a0556e4f8c0aab77d4c4ddc41c
Sloont <colinabel93@gmail.com>
2021-04-28 14:05:39 -0400

    added test conditions to makeGame

--- scripts/makeGame.js
+++ scripts/makeGame.js
@@ -1,2 +1,20 @@
-makeGame.js
+// class used for making a game
 
+class Game {
+
+    constructor(home, away) {
+        this.home = home;
+        this.away = away;
+
+    }
+
+    swapSides = () => {
+        let temp = this.home;
+        this.home = this.away;
+        this.away = temp;
+    }
+}
+const team1 = {"teamName": "benders"};
+const team2 = {"teamName": "frylocks"};
+const game1 = new Game(team1, team2);
+console.log(game1);
