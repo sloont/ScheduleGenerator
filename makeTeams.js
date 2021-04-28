4a11fdd0e422d2634e1cb8ca6469db46cb0f1a15
Sloont <colinabel93@gmail.com>
2021-04-28 13:00:07 -0400

    added makeTeams.js which creates the Team objects and saves them in an Array (for now) of Team objects

---
+++ makeTeams.js
@@ -0,0 +1,25 @@
+//test script to make teams and add them to an array
+
+let numberOfTeams = 0;
+let teamList = [];
+const saveTeamBtn = document.getElementById("saveTeamBtn");
+
+const saveTeam = () => {
+
+    const teamName = document.getElementsByName("teamName")[0].value;
+    const teamColor = document.getElementsByName("teamColor")[0].value;
+    const teamLogo = document.getElementsByName("teamLogo")[0].value;
+
+    numberOfTeams++;
+
+    teamList.push(new Team(numberOfTeams, teamName, teamColor, teamLogo));
+
+
+}
+
+saveTeamBtn.addEventListener("click", () => {
+    testFunc();
+    saveTeam();
+    console.log(teamList);
+});
+
