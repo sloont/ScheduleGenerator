a790e7135a89b65febc5e3b06678e7c94b6ea020
Sloont <ColinAbel93@gmail.com>
2021-04-29 21:24:26 -0400

    removed the old scheduler file and renamed test.js to scheduler.js

--- scripts/scheduler.js
+++ scripts/scheduler.js
@@ -1,38 +1,20 @@
-// A class used to schedule an array of teams based on a set of constraints
-class Scheduler {
+// A class to store information related to a Team
+
+class Team {
     /**
      * Constructor
-     * @param {Array} teams An array of Team objects used to schedule
+     * @param {Number} teamNumber The team number
+     * @param {String} teamName The name of the team
+     * @param {String} teamColor The color of the team
+     * @param {String} teamLogo The logo of the team
      */
-    constructor(teams) {
-      this.teams = teams;
+    constructor(teamNumber, teamName, teamColor, teamLogo) {
+      this.teamNumber = teamNumber;
+      this.teamName = teamName;
+      this.teamColor = teamColor;
+      this.teamLogo = teamLogo;
+
   
-      this.constraints = [];
-    }
-  
-    /**
-     * A method that internally stores a constraint and then uses it to evaluate any future schedule
-     * @param {Function} constraintFunction A Function object that takes in an Array of teams and outputs a boolean specifying whether the constraint is met
-     */
-    addConstraint = (constraintFunction) => {
-      this.constraints.push(constraintFunction);
-    }
-  
-    /**
-     * A method creates a schedule
-     * @param {Number} gameweeks The number of gameweeks in the schedule
-     */
-    createSchedule = (gameweeks) => {
-      let schedule = new Schedule(this.teams, gameweeks);
-  
-      for (let gameweek = 0; gameweek < gameweeks; gameweek++) {
-        const team1Number = Math.floor(Math.random() * this.teams.length);
-        const team2Number = Math.floor(Math.random() * this.teams.length);
-        const game = { H: this.teams[team1Number], A: this.teams[team2Number] };
-        schedule.addGameToGameweek(game, gameweek);
-      }
-  
-      console.log(schedule.schedule);
     }
   
   }
