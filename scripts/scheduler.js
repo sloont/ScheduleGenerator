14e817296897cc1bc94ef77e7084566527047b7f
Sloont <ColinAbel93@gmail.com>
2021-04-29 21:42:32 -0400

    accidentally named teams to scheduler instead of test. fixed this. in the middle of working on css

--- scripts/scheduler.js
+++ scripts/scheduler.js
@@ -1,20 +1,56 @@
-// A class to store information related to a Team
+//Class to represent the scheduler
 
-class Team {
+class Scheduler {
+
     /**
-     * Constructor
-     * @param {Number} teamNumber The team number
-     * @param {String} teamName The name of the team
-     * @param {String} teamColor The color of the team
-     * @param {String} teamLogo The logo of the team
-     */
-    constructor(teamNumber, teamName, teamColor, teamLogo) {
-      this.teamNumber = teamNumber;
-      this.teamName = teamName;
-      this.teamColor = teamColor;
-      this.teamLogo = teamLogo;
+    *  Constructor
+    * @param {Array} teams An array of Team objects that participate in the schedule
+    */
+    constructor(teams) {
+        this.teams = teams;
 
-  
+        this.constraints = [];
+        this.teamsWithGames = [];
+        this.teamsNeedGames = [];
+        this.schedule = [];
+
+        for (let x = 0; x < this.teams.length; x++) {
+            this.teamsNeedGames.push(this.teams[x]);
+        }
     }
-  
-  }
+
+    /**
+     * 
+     * @param {Number} gameweeks The number of the gameweek (or the index into the internal schedule array)
+     */
+    createSchedule = (gameweeks) => {
+        
+        const schedule = new Schedule(this.teams, gameweeks);
+
+        for (let i = 0; i < gameweeks; i++) {
+            
+            while (this.teamsNeedGames.length > 0) {
+
+                let team = this.teamsNeedGames[0];
+
+                schedule.addGameToGameweek({H: team, A: this.teamsNeedGames[1]}, i);
+
+                this.teamsWithGames.push(team);
+                this.teamsWithGames.push(this.teamsNeedGames[1]);
+
+                this.teamsNeedGames.splice(0,1);
+                console.log(this.teams.length);
+                console.log(this.teamsNeedGames.length);
+                this.teamsNeedGames.splice(0,1);
+               
+            }
+        }
+
+
+
+
+        console.log(schedule);
+    }
+}
+
+
---
+++ scripts/team.js
@@ -0,0 +1,20 @@
+// A class to store information related to a Team
+
+class Team {
+    /**
+     * Constructor
+     * @param {Number} teamNumber The team number
+     * @param {String} teamName The name of the team
+     * @param {String} teamColor The color of the team
+     * @param {String} teamLogo The logo of the team
+     */
+    constructor(teamNumber, teamName, teamColor, teamLogo) {
+      this.teamNumber = teamNumber;
+      this.teamName = teamName;
+      this.teamColor = teamColor;
+      this.teamLogo = teamLogo;
+
+  
+    }
+  
+  }
