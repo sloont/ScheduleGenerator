4e1baf8223bded1a3fd605de11790c5050fbc06c
Sloont <colinabel93@gmail.com>
2021-05-05 16:33:01 -0400

    added noTeamObjects which is currently a copy of generator.js. going to remove the actual team objects from the guts of the generator class to save memory. we will just use numbers

--- scripts/asdf.js
+++
@@ -1,9 +0,0 @@
-addGameToGameWeek = () => {
-    const team = this.teams[0];
-    const teamGames = this.gamePool.get(team.teamNumber);
-    const gameChoice = teamGames.get(Object.away.teamNumber);
-
-    console.log(team);
-    console.log(teamGames);
-    console.log(gameChoice);
-}
